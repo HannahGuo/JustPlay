eb9583a754c6fd20f4d916cc452ba823f4266391
Hannah Guo <hguo2102@gmail.com>
2018-06-23 20:46:54 -0400

    Latest commit

--- CE_Summative.ino
+++ CE_Summative.ino
@@ -20,17 +20,17 @@ const LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int score = 0; // Keeps count of score
 
 // Various Songs, stored as integer arrays.
-int marySong[] = {3, 2, 1, 2, 3, 3, 3, 2, 2, 2, 3, 5, 5, 3, 2, 1, 2, 3, 3, 3, 3, 2, 2, 3, 2, 1};
-int hotCrossSong[] = {3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 2, 2, 3, 2, 1};
-int scale[] = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1};
-int jingle[] = {3, 3, 3, 3, 3, 3, 3, 5, 1, 2, 3, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 2, 3, 2, 5};
-int spider[] = {5, 1, 1, 1, 2, 3, 3, 3, 2, 3, 1, 3, 3, 4, 5, 5, 4, 3, 4, 5, 3, 1, 1, 2, 3, 3, 2, 1, 2, 3, 1, 5, 5, 1, 1, 1, 2, 3, 3, 3, 2, 1, 2, 3, 1};
+int marySong[] = {3, 2, 1, 2, 3, 3, 3, 2, 2, 2, 3, 5, 5, 3, 2, 1, 2, 3, 3, 3, 3, 2, 2, 3, 2, 1}; // mary had a little lamb
+int hotCrossSong[] = {3, 2, 1, 3, 2, 1, 3, 3, 3, 2, 2, 2, 3, 2, 1}; // hot cross buns
+int scale[] = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}; // scale
+int jingle[] = {3, 3, 3, 3, 3, 3, 3, 5, 1, 2, 3, 4, 4, 4, 4, 3, 3, 3, 3, 3, 2, 2, 3, 2, 5}; // jingle bells
+int spider[] = {5, 1, 1, 1, 2, 3, 3, 3, 2, 3, 1, 3, 3, 4, 5, 5, 4, 3, 4, 5, 3, 1, 1, 2, 3, 3, 2, 1, 2, 3, 1, 5, 5, 1, 1, 1, 2, 3, 3, 3, 2, 1, 2, 3, 1}; // itsy bitsy spider
 
 // An array that saves the letter values of the notes
 const String noteValues[] = {"C", "D", "E", "F", "G"};
 
 void setup() { // runs once when the Arduino starts up
-  // Set each button as an input
+  // Set each button as an input. Note that when these pins are read from, a LOW output means that the button was pressed, and a HIGH output means it wasn't.
   pinMode(redButton, INPUT_PULLUP);
   pinMode(yellowButton, INPUT_PULLUP);
   pinMode(greenButton, INPUT_PULLUP);
@@ -56,7 +56,7 @@ void playSong(int *song) {   // this function plays the song passed in as a para
     while (millis() - currentMill < 2000) {                           // this loop acts as a timer, since it will run until about 2 seconds has passed.
       if ((digitalRead(song[i] + 1) == LOW) and oneButtonPressed()) { // check if the user hit the correct button, and only one button was pressed at the time
         playNote(song[i]);     // play the note on the buzzer
-        digitalWrite(1, HIGH); // turn the LED on
+        digitalWrite(1, HIGH); // turn the LED on (HIGH)
         correctPress = true;   // set correctPress to true
         break;                 // break out of this time loop
       }
@@ -97,16 +97,16 @@ void mainInterface() { // This function is the main interface of the program
   lcd.setCursor(0, 0);  // Set the location of the cursor to the start of the first tow
   lcd.print("Select");  // Print select to the LCD
   if (oneButtonPressed()) { // Check if only one button was pressed
-    // The following code plays a song based on the button pressed. 
-    if (digitalRead(redButton) == LOW) {
+    // The following code plays a song based on the button pressed.
+    if (digitalRead(redButton) == LOW) { // Check if the red wired button was pressed
       playSong(marySong); // plays Mary had a Little Lamb
-    } else if (digitalRead(yellowButton) == LOW) {
+    } else if (digitalRead(yellowButton) == LOW) {  // Check if the yellow wired button was pressed
       playSong(hotCrossSong); // plays Hot Cross Buttons
-    } else if (digitalRead(greenButton) == LOW) {
+    } else if (digitalRead(greenButton) == LOW) {  // Check if the green wired button was pressed
       playSong(scale); // plays a simple scale
-    } else if (digitalRead(blueButton) == LOW) {
+    } else if (digitalRead(blueButton) == LOW) {  // Check if the blue wired button was pressed
       playSong(jingle); // plays Jingle Bells
-    } else if (digitalRead(whiteButton) == LOW) {
+    } else if (digitalRead(whiteButton) == LOW) {  // Check if the white wired button was pressed
       playSong(spider); // plays the itsy bitsy spider
     }
   }
